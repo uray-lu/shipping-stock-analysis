4673190867f30d65427f0b44fd6c6ef002a857d0
Ray <george02270227@gmail.com>
2022-08-16 15:41:51 +0800

    Just test. Done.

--- Data/get_close_price.py
+++ Data/get_close_price.py
@@ -7,6 +7,9 @@ Created on Sun Jun  5 17:12:52 2022
 """
 
 from pandas_datareader import data as pdr
+from colorama import Back, init
+
+init(autoreset = True)
 
 class GetStockData():
     
@@ -21,12 +24,18 @@ class GetStockData():
         data.columns.names = [None]
         data.index = data.index.astype(str)
         
-        print('Already get all Data.')
+        print(f"{'Get Data' : <10}{'·'*20 : ^10}{Back.GREEN}{'Done' : ^10}")
         
         return data    
     
     def StoreData(self):
         store_data = self.MakeOutPut()
-        store_data.to_csv('Data/Close_price_data.csv', index = False)
+        store_data.to_csv('Close_price_data.csv', index = False)
         
-        print('Data stored Finished!!')
+        print(f"{'Data Store' : <10}{'·'*20 : ^10}{Back.GREEN}{'Done' : ^10}")
+        
+        
+        
+stock_lists = ['2609.TW', '2603.TW', '2615.TW', '5608.TW', '2605.TW','2606.TW', '2637.TW' ]
+stocks_data = GetStockData('2021-01-01', '2022-01-01', stock_lists)
+stocks_data.StoreData()
